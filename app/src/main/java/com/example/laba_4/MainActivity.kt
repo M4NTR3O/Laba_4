19a52491aaf2715314a474621f980e93e49dd741
emil_ <emil.kushner.stud@mail.ru>
2023-10-04 19:35:56 +0700

    Добавление полей Button

--- app/src/main/java/com/example/laba_4/MainActivity.kt
+++ app/src/main/java/com/example/laba_4/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
     }
 }
