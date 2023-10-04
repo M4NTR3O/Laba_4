1a6775baad8ba6ca3bc156716a97f06e2a03b9b0
emil_ <emil.kushner.stud@mail.ru>
2023-10-04 19:41:17 +0700

    Назначение слушателя для кнопок

--- app/src/main/java/com/example/laba_4/MainActivity.kt
+++ app/src/main/java/com/example/laba_4/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.laba_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -11,5 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        trueButton.setOnClickListener { view: View ->
+            // Что-то выполнить после нажатия
+        }
+        falseButton.setOnClickListener { view: View ->
+            // Что-то выполнить после нажатия
+        }
     }
 }
