1ac295526b3907c525b47d3e0c9998d704d74d46
emil_ <emil.kushner.stud@mail.ru>
2023-10-05 17:03:37 +0700

    Добавление класса Question

--- app/src/main/java/com/example/laba_4/MainActivity.kt
+++ app/src/main/java/com/example/laba_4/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.laba_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
