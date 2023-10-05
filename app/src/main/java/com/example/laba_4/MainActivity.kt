fdd2a27a9103a1bc94eecefc0b4a946de8b6e98c
emil_ <emil.kushner.stud@mail.ru>
2023-10-05 18:38:34 +0700

    Подключение виджета TextView + Подключение новой кнопки

--- app/src/main/java/com/example/laba_4/MainActivity.kt
+++ app/src/main/java/com/example/laba_4/MainActivity.kt
@@ -2,14 +2,16 @@ package com.example.laba_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans,true),
@@ -23,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
         trueButton.setOnClickListener { view: View ->
             // Что-то выполнить после нажатия
             Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
@@ -31,5 +35,13 @@ class MainActivity : AppCompatActivity() {
             // Что-то выполнить после нажатия
             Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
         }
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
+        }
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+
     }
 }
