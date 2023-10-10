5c27d7f730dcc57c2bd8b60878e74858431c9fc6
emil_ <emil.kushner.stud@mail.ru>
2023-10-11 00:39:38 +0700

    Добавлены невидимость кнопок

--- app/src/main/java/com/example/laba_4/MainActivity.kt
+++ app/src/main/java/com/example/laba_4/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.laba_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -14,7 +16,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
-    private lateinit var prevButton: Button
     private lateinit var questionTextView: TextView
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -35,24 +36,39 @@ class MainActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
         trueButton.setOnClickListener {
             checkAnswer(true)
+            trueButton.visibility= View.INVISIBLE
+            falseButton.visibility= View.INVISIBLE
             result_User += 1
         }
         falseButton.setOnClickListener {
             checkAnswer(false)
+            trueButton.visibility= View.INVISIBLE
+            falseButton.visibility= View.INVISIBLE
         }
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            trueButton.visibility= View.VISIBLE
+            falseButton.visibility= View.VISIBLE
             updateQuestion()
-        }
-        prevButton.setOnClickListener {
-            currentIndex = (currentIndex + questionBank.size - 1) % questionBank.size
-            updateQuestion()
+            if (currentIndex == 0){
+                nextButton.visibility= View.INVISIBLE
+            }
         }
         updateQuestion()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        outState.putInt("cur_key", currentIndex)
+        outState.putInt("result", result_User)
+        updateQuestion()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        currentIndex = savedInstanceState!!.getInt("cur_key");
+        updateQuestion()
+        result_User = savedInstanceState.getInt("result")
     }
     override fun onStart() {
         super.onStart()
