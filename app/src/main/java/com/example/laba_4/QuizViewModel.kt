eb69a3035f679b5a3fa1521d54ddfd588d10eb31
emil_ <emil.kushner.stud@mail.ru>
2023-10-23 22:37:04 +0700

    Вставка данных модели в QuizViewModel

--- app/src/main/java/com/example/laba_4/QuizViewModel.kt
+++ app/src/main/java/com/example/laba_4/QuizViewModel.kt
@@ -6,12 +6,12 @@ import androidx.lifecycle.ViewModel
 private const val TAG = "QuizViewModel"
 
 class QuizViewModel : ViewModel() {
-    init {
-        Log.d(TAG, "ViewModel instance created")
-    }
-    override fun onCleared() {
-        super.onCleared()
-        Log.d(TAG,
-            "ViewModel instance about to be destroyed")
-    }
+        private val questionBank = listOf(
+            Question(R.string.question_australia, true),
+            Question(R.string.question_oceans,true),
+            Question(R.string.question_mideast,false),
+            Question(R.string.question_africa,false),
+            Question(R.string.question_americas, true),
+            Question(R.string.question_asia,true))
+        var currentIndex = 0
 }
