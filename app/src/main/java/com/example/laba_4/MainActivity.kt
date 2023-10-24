f7bea52e0c5e4ec2bcfe301b9639dd9397869c2f
emil_ <emil.kushner.stud@mail.ru>
2023-10-24 23:59:58 +0700

    получение результата от дочерней функции + назначение результата + обработка результата

--- app/src/main/java/com/example/laba_4/MainActivity.kt
+++ app/src/main/java/com/example/laba_4/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.laba_4
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,6 +17,7 @@ import androidx.lifecycle.ViewModelProviders
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
 private const val KEY_RESULT = "result"
+private const val REQUEST_CODE_CHEAT = 0
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -62,16 +64,28 @@ class MainActivity : AppCompatActivity() {
             trueButton.visibility= View.VISIBLE
             falseButton.visibility= View.VISIBLE
             quizViewModel.moveToNext()
+            if(quizViewModel.currentIndex == quizViewModel.questionBank.size - 1){
+                nextButton.visibility= View.INVISIBLE
+            }
             updateQuestion()
         }
         cheatButton.setOnClickListener {
             val answerIsTrue = quizViewModel.currentQuestionAnswer
             val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
-            startActivity(intent)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
         }
         updateQuestion()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK){
+            return
+        }
+        if (resultCode == REQUEST_CODE_CHEAT){
+            quizViewModel.isCheater = data?.getBooleanExtra(EXTRA_ANSWER_SHOWN,false)?:false
+        }
+    }
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         super.onSaveInstanceState(savedInstanceState)
         Log.i(TAG, "onSaveInstanceState")
@@ -105,10 +119,10 @@ class MainActivity : AppCompatActivity() {
     }
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = quizViewModel.currentQuestionAnswer
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+        val messageResId =  when{
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
         if (userAnswer == correctAnswer){
             quizViewModel.result += 1
