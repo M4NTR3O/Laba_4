ae5003a8196245dbe5b4008041128a4648ddf75c
emil_ <emil.kushner.stud@mail.ru>
2023-10-24 23:33:53 +0700

    редаченный дизайн + запуск читактивити с дополнениями

--- app/src/main/java/com/example/laba_4/MainActivity.kt
+++ app/src/main/java/com/example/laba_4/MainActivity.kt
@@ -65,7 +65,8 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
         cheatButton.setOnClickListener {
-            val intent = Intent(this, CheatActivity::class.java)
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
             startActivity(intent)
         }
         updateQuestion()
